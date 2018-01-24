516c76d677c81148e27111bd698ead7199b1e3c9
Teerapat12 <teerapat.time12@gmail.com>
2018-01-24 15:33:39 +0200

    Fix little bit more

---
+++ Assets/Scripts/EnemyOnStepped.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyOnStepped : MonoBehaviour {
+
+	public float bounceForce = 20f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		//Check if character
+		if (coll.gameObject.tag == "Nelle" || coll.gameObject.tag=="Poro" || coll.gameObject.tag=="Slow")
+			
+			//Add force to character upward a bit to hsve a mario like effect.
+			coll.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, bounceForce));
+			//Die here. Call the overall enemy rather than the head.
+			GetComponentInParent<EnemyController> ().Die();
+			
+	}
+		
+}
